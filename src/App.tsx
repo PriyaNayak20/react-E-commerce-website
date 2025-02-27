1f4ed19aa53df60c571f27bf931ac3341e341a83
PriyaNayak20 <priya.n@webkorps.com>
2025-02-28 00:53:42 +0530

    file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Sidebar from './commponents/Sidebar'
+import MainContent from './commponents/MainContent'
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
---
+++ src/commponents/MainContent.tsx
@@ -0,0 +1,170 @@
+import { useState, useEffect } from 'react'
+import { useFilter } from './FilterContext'
+import { Tally3 } from 'lucide-react'
+import axios from 'axios'
+import BookCard from './BookCard'
+const MainContent = () => {
+  const { searchQuery, SelectedCategory, minPrice, maxPrice, keyword } =
+    useFilter()
+
+  const [products, setProducts] = useState<any[]>([])
+  const [filter, setFilter] = useState('all')
+  const [currentPage, setCurrentPage] = useState(1)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const itemsPerPage = 12
+
+  useEffect(() => {
+    let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${
+      (currentPage - 1) * itemsPerPage
+    }`
+
+    if (keyword) {
+      url = `https://dummyjson.com/products/search?q=${keyword}`
+    }
+
+    axios
+      .get(url)
+      .then((response) => {
+        setProducts(response.data.products)
+        console.log(response.data.products)
+      })
+      .catch((error) => {
+        console.log('Error fetching data', error)
+      })
+  }, [currentPage, keyword])
+
+  const getFilteredProducts = () => {
+    let filteredProducts = products
+
+    if (SelectedCategory) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.category === SelectedCategory
+      )
+    }
+
+    if (minPrice !== undefined) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price >= minPrice
+      )
+    }
+
+    if (maxPrice !== undefined) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.price <= maxPrice
+      )
+    }
+
+    if (searchQuery) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+      )
+    }
+
+    switch (filter) {
+      case ' expensive':
+        return filteredProducts.sort((a, b) => b.price - a.price)
+
+      case 'cheap':
+        return filteredProducts.sort((a, b) => a.price - b.price)
+      case 'popular':
+        return filteredProducts.sort((a, b) => b.rating - a.rating)
+
+      default:
+        return filteredProducts
+    }
+  }
+  const totalProducts = 100
+  const totalPages = Math.ceil(totalProducts / itemsPerPage)
+  const handlePageChange = (page: number) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page)
+    }
+  }
+
+  const getPaginationButton = () => {
+    const buttons: number[] = []
+    let startPage = Math.max(1, currentPage - 2)
+    let endPage = Math.min(totalPages, currentPage + 2)
+
+    if (currentPage - 2 < 1) {
+      endpage = Math.min(totalPages, endPage + (2 - currentPage - 1))
+    }
+    if (currentPage + 2 > totalPages) {
+      startPage = Math.min(1, startPage - (2 - totalPages - currentPage))
+    }
+  }
+
+  const filteredProductsList = getFilteredProducts()
+
+  return (
+    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+      <div className="mb-5">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <div className="realtive mb-5 mt-5">
+            <button className="border px-4 py-2 rounded-full flex items-center">
+              <Tally3 className="mr-2" />
+              {filter === 'all'
+                ? 'Filter'
+                : filter.charAt(0).toLowerCase() + filter.slice(1)}
+            </button>
+
+            {dropdownOpen && (
+              <div className="absolute bg-white border-gray-300 rounded mt-2 w-full sm:w-40">
+                <button
+                  onClick={() => setFilter('cheap')}
+                  className="block px-4 py-2 w-full text-left hover:bg-gray-200"
+                >
+                  Cheap
+                </button>
+                <button
+                  onClick={() => setFilter('Expensive')}
+                  className="block px-4 py-2 w-full text-left hover:bg-gray-200"
+                >
+                  Expensive
+                </button>
+                <button
+                  onClick={() => setFilter('Popular')}
+                  className="block px-4 py-2 w-full text-left hover:bg-gray-200"
+                >
+                  Popular
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5">
+          {filteredProductsList.map((product) => (
+            <BookCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              image={product.thumbnail}
+              price={product.price}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col sm:flex-row justify-between item-center mt-5">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            className="border px-4 py-2 mx-2 rounded-full"
+          >
+            Previous
+          </button>
+          <div className="flex flex-wrap justify-center">
+            {getPaginationButton()}
+          </div>
+
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            className="border px-4 py-2 mx-2 rounded-full"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default MainContent
